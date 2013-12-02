ec4f74a64264e4bcd1c8c7bc411d4f0efd63d9fc
JEFERSON LUIS MORAES GONÇALVES <jlmg070542@gmail.com>
2013-12-01 23:42:54 -0200

    Create InimigoCase.java

---
+++ Trabalho04_Labirinto/InimigoCase.java
@@ -0,0 +1,24 @@
+public class InimigoCase {
+
+	
+
+	public String Tipo() {
+		
+		return null;
+	}
+
+	public int ataque(String string) {
+		
+		return 0;
+	}
+
+	public int vida(String tipo) {
+	
+		return 0;
+	}
+	public int tipoInimigo(String tipo) {
+		
+		return 0;
+
+}
+}
